e7b44bd592a259ad1145d6437c3b7df27e05899c
SativusOxide <113673249+SativusOxide@users.noreply.github.com>
2022-09-16 19:04:58 +1000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,91 @@
+let overlay1 = document.getElementById("overlay1")
+let overlay2 = document.getElementById("overlay2")
+let overlay3 = document.getElementById("overlay3")
+let overlay4 = document.getElementById("overlay4")
+let overlay5 = document.getElementById("overlay5")
+let overlay6 = document.getElementById("overlay6")
+let overlay7 = document.getElementById("overlay7")
+
+/*      overlay1        */
+
+function on() {
+    overlay1.style.display = "flex";
+    overlay1.style.justifyContent = "center";
+    overlay1.style.alignItems = "center";
+}
+
+/*      Overlay2        */
+
+function off() {
+    document.getElementById("overlay1").style.display = "none";
+  }
+
+function on1() {
+    overlay2.style.display = "flex";
+    overlay2.style.justifyContent = "center";
+    overlay2.style.alignItems = "center";
+}
+
+function off1() {
+    document.getElementById("overlay2").style.display = "none";
+}
+
+/*      Overlay3        */
+
+function on2() {
+    overlay3.style.display = "flex";
+    overlay3.style.justifyContent = "center";
+    overlay3.style.alignItems = "center";
+}
+
+function off2() {
+    document.getElementById("overlay3").style.display = "none";
+}
+
+/*      Overlay4        */
+
+function on3() {
+    overlay4.style.display = "flex";
+    overlay4.style.justifyContent = "center";
+    overlay4.style.alignItems = "center";
+}
+
+function off3() {
+    document.getElementById("overlay4").style.display = "none";
+}
+
+/*      Overlay5        */
+
+function on4() {
+    overlay5.style.display = "flex";
+    overlay5.style.justifyContent = "center";
+    overlay5.style.alignItems = "center";
+}
+
+function off4() {
+    document.getElementById("overlay5").style.display = "none";
+}
+
+/*      Overlay6        */
+
+function on5() {
+    overlay6.style.display = "flex";
+    overlay6.style.justifyContent = "center";
+    overlay6.style.alignItems = "center";
+}
+
+function off5() {
+    document.getElementById("overlay6").style.display = "none";
+}
+
+/*      Overlay7        */
+
+function on6() {
+    overlay7.style.display = "flex";
+    overlay7.style.justifyContent = "center";
+    overlay7.style.alignItems = "center";
+}
+
+function off6() {
+    document.getElementById("overlay7").style.display = "none";
+}
